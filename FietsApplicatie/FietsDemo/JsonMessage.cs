b8573dfde37c34b06fe0b823b5fe0d091f52127f
Momin2004 <m.musa@student.avans.nl>
2022-09-29 14:45:44 +0200

    convert method uses classes

---
+++ FietsApplicatie/FietsDemo/JsonMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FietsDemo
+{
+
+    public class JsonData
+    {
+        public string ID { get; set; }
+        public string Username { get; set; }
+        public double Speed { get; set; }
+        public int Heartrate { get; set; }
+
+    }
+}
