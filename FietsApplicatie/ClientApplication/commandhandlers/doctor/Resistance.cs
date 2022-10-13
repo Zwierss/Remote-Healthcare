673013be615b42920027d3f8f19412ae8f5ccb1b
Momin2004 <m.musa@student.avans.nl>
2022-10-13 22:55:37 +0200

    added SetResistance command & method

---
+++ FietsApplicatie/ClientApplication/commandhandlers/doctor/Resistance.cs
@@ -0,0 +1,18 @@
+﻿using FietsDemo;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApplication.commandhandlers.doctor
+{
+    public class Resistance : ICommand
+    {
+        public void OnCommandReceived(JObject packet, Client parent)
+        {
+
+        }
+    }
+}
