e13c0f771363657e0af8060bdb44d457d7001ac0
Bram Laros <bramlaros@gmail.com>
2022-10-15 00:43:00 +0200

    Placed more trees

--- FietsApplicatie/VirtualReality/components/Tree.cs
+++ FietsApplicatie/VirtualReality/components/Tree.cs
@@ -17,14 +17,37 @@ public class Tree
     public void PlaceTrees()
     {
         List<float[]> r = new();
+        //House south
         r.Add(new float[] { -62, -10 });
         r.Add(new float[] { -62, 10 });
-        r.Add(new float[] { -30, -60 });
-        r.Add(new float[] { -30, -75 });
-        r.Add(new float[] { -30, -80 });
-        r.Add(new float[] { -32, -72 });
-        
-        r.Add(new float[] { -35, -70 });
+        r.Add(new float[] { -64, -10 });
+        r.Add(new float[] { -64, 10 });
+        r.Add(new float[] { -66, -10 });
+        r.Add(new float[] { -66, 10 });
+        r.Add(new float[] { -68, -10 });
+        r.Add(new float[] { -68, 10 });
+        r.Add(new float[] { -70, -10 });
+        r.Add(new float[] { -70, 10 });
+        r.Add(new float[] { -74, 10 });
+        r.Add(new float[] { -72, 10 });
+        r.Add(new float[] { -74, -10 });
+        r.Add(new float[] { -74, 10 });
+        r.Add(new float[] { -63, -12 });
+        r.Add(new float[] { -65, -12 });
+        r.Add(new float[] { -67, -12 });
+        r.Add(new float[] { -69, -12 });
+        r.Add(new float[] { -71, -12 });
+
+        r.Add(new float[] { -62, -12 });
+        r.Add(new float[] { -64, -13 });
+        r.Add(new float[] { -62, -14 });
+        r.Add(new float[] { -64, -15 });
+        r.Add(new float[] { -61, -16 });
+        r.Add(new float[] { -63, -17 });
+        r.Add(new float[] { -61, -18 });
+        r.Add(new float[] { -63, -19 });
+        r.Add(new float[] { -61, -20 });
+        r.Add(new float[] { -63, -21 });
         
 
 
@@ -37,8 +60,8 @@ public class Tree
         float[] heights = _parent.Heights;
         for (int i = 0; i < heights.Length; i++)
         {
-            int type = new Random().Next(8);
-            if (type == 8) type = 10;
+            int type = new Random().Next(9) + 1;
+            
             _parent.SendTunnel("scene/node/add", new
             {
                 name = "tree-" + Guid.NewGuid(),
