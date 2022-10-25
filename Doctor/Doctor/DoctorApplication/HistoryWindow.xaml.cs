0842fb9ffd4a1d3be9728ec11c9e9eda248e7375
Zwiers <at.zwiers@student.avans.nl>
2022-10-25 02:20:32 +0200

    created better view and selection for view models

---
+++ Doctor/Doctor/DoctorApplication/HistoryWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoctorApplication
+{
+    /// <summary>
+    /// Interaction logic for HistoryWindow.xaml
+    /// </summary>
+    public partial class HistoryWindow : UserControl
+    {
+        public HistoryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
